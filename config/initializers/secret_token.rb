31c3730b9a67070955691d2561c94be30c7fe8b4
Roberto C. Rivera <rivera.roberto89@gmail.com>
2013-11-12 08:41:12 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thundercats::Application.config.secret_key_base = '71e414627ca2e990008aa02067707b99418815d78f49985a4294744b3e968768aac571043dc2dccb9d2ee3456c88448c6c4fc22614a9f49009536f1fdac75e11'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Thundercats::Application.config.session_store :cookie_store, key: '_thundercats_session'
